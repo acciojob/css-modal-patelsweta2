8dd3dfab8996f08121ae5b26054f77b6ab5403ac
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-01-25 13:41:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const modal = doument.getElementById('myModal');
+const openBtn = document.getElementById('openModal');
+
+openBtn.addEventListener('click', openModal);
+
+window.addEventListener('click', (event) => {
+	if(event.target === modal){
+		closeModal();
+	}
+});
+
+function openModal() {
+	modal.style.display = 'flex';
+}
+function closeModal() {
+	modal.style.display = 'none';
+}
